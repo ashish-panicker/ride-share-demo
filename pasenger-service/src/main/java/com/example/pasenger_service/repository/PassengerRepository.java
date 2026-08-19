27d839d86e3a574ef7d6f9a4665baf0a2ca769d8
ashish-panicker <ashish.s.panicker@outlook.com>
2026-08-19 14:54:09 +0530

    Remove @Repository annotation and add Service Layer and DTO for Passenger Service

--- pasenger-service/src/main/java/com/example/pasenger_service/repository/PassengerRepository.java
+++ pasenger-service/src/main/java/com/example/pasenger_service/repository/PassengerRepository.java
@@ -2,8 +2,6 @@ package com.example.pasenger_service.repository;
 
 import com.example.pasenger_service.entity.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PassengerRepository extends JpaRepository<Passenger, Long> {
 }
